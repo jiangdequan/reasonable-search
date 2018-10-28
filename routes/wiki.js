3dcccdd1bcc339b81c25c4ef1325a14876cd387d
jiangdequan <jiangdequan1314@gmail.com>
2018-10-28 14:27:22 +0800

    add wiki

---
+++ routes/wiki.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var request = require('request');
+
+// 初始化express
+var router = express.Router();
+
+// 路由
+router.get('/wiki/:text', function(req, res) {
+    var text = req.params.text;
+    var url = 'https://zh.wikipedia.org/wiki/' + text;
+    req.pipe(request(url)).pipe(res);
+});
+
+exports.router = router;
