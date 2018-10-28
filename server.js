475072603460868240377c9c134039a0171db6b1
jiangdequan <jiangdequan1314@gmail.com>
2018-10-28 14:28:38 +0800

    add a function of soccer

--- server.js
+++ server.js
@@ -7,8 +7,10 @@
 var express = require('express');
 
 var google = require('./routes/google');
+var wiki = require('./routes/wiki');
 var ssr = require('./routes/ssr-subscribe');
 var proxy = require('./routes/online-proxy');
+var soccer = require('./routes/soccer');
 
 var LOGGER = require('./lib/logger/logger');
 
@@ -24,8 +26,10 @@ app.set('view engine', 'ejs');
 
 // routes
 app.use('/', google.router);
+app.use('/', wiki.router);
 app.use('/', ssr.router);
 app.use('/', proxy.router);
+app.use('/', soccer.router);
 
 // port and ip
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
@@ -33,10 +37,12 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 // start the server and listen
 app.listen(port, ip, function () {
-    ssr.site52ssr();
-    ssr.siteDoub();
-    ssr.siteFreeSs();
+    // ssr.site52ssr();
+    // ssr.siteDoub();
+    // ssr.siteFreeSs();
     LOGGER.info('Start to fetch ss/ssr config from internet...', __filename);
+    soccer.siteHupu();
+    LOGGER.info('Start to fetch matches data from internet...', __filename);
 });
 
 LOGGER.info('Server running on http://' + ip + ':' + port, __filename);
